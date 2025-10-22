b42e22c6ca2f903c085445c9712aa31618b78b2f
Imad <muhdimadduddin04@gmail.com>
2025-10-22 15:39:00 +0800

    Add industry section

---
+++ industry.py
@@ -0,0 +1,11 @@
+INDUSTRY_OPTIONS = [
+    "--- Select an Industry ---",
+    "E-commerce",
+    "Services (e.g., Bootcamp, Agency)",
+    "Physical Retail",
+    "Non-Profit (Donations)",
+    "B2B (Business-to-Business)",
+    "Healthcare",
+    "Food & Beverage (F&B)",
+    "Tech & SaaS"
+]
